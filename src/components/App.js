72239e5947ec54f872a8e38a1ac45b463f5d9ce5
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-01-08 17:32:10 +0200

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
@@ -24,6 +24,12 @@ export default function App() {
           },
         ]);
   };
+
+  useEffect(() => {
+    console.log('update information');
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
 
   const handleChange = evt => {
     setFilter(evt.target.value);
